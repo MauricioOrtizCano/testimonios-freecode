6793d38531662c92a78b6347bb75fc176524a589
MauricioOrtizCano <cmoc14@yahoo.com>
2022-11-02 09:16:05 -0500

    Clone Testimonios FreeCode

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -7,7 +7,7 @@ export default function Testimonio(props) { //componente funcional porque esta c
             <img 
                 className="imagen-testimonio"
                 src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-                alt="Foto de Emma"
+                alt={`Foto de ${props.imagen}`}
             />
             <div className="contenedor-texto-testimonio">
                 <p className="nombre-testimonio"><b>{props.nombre}</b> en {props.pais}</p>
